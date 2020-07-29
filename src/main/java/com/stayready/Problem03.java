548a51dd1e8ac04514c8c32eb12348d9df738e48
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-29 09:24:22 -0400

    ready

--- src/main/java/com/stayready/Problem03.java
+++ src/main/java/com/stayready/Problem03.java
@@ -1,4 +1,57 @@
 package com.stayready;
 
 public class Problem03 {
+    //create a function to determine whether too many swaps were made
+    public Boolean swapChecks(Integer[] array, Integer numOfSwaps)
+    {
+        Boolean tooManySwaps = false;
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] - i -1 > numOfSwaps)  //if the array at ith element minus the index minus 1 (for offset) is less than 2
+            {
+                tooManySwaps = true; //set too many swaps to true
+            }
+        }
+        return tooManySwaps;
+    }
+
+    //create a function to count the swaps
+    public Integer countSwaps(Integer[] array)
+    {
+        if(swapChecks(array, 2)== true)
+        {
+            return -1; //if there are too many swaps return -1
+        }
+        Integer swaps = 0;
+        for (int i = 0; i < array.length-1; i++)
+        {
+            if (array[i+1] != array[i]+1) //if the next element isn't equal to the current element plus 1
+            {
+                swaps++;   // increment swaps
+            }
+        }
+        return swaps;
+    }
+    //The assignment also asks for returning a string i will create a method that returns String
+    public String countSwapsv2(Integer[] array)
+    {
+        String swaps = "";
+        if (swapChecks(array, 2)== true)
+        {
+            swaps = "Too Chaotic";
+            return swaps;
+        }
+        Integer count = 0;
+        for (int i = 0; i < array.length-1; i++)
+        {
+            if (array[i+1] != array[i] +1)
+            {
+                count ++;
+
+            }
+        }
+        swaps = count.toString();
+        return swaps;
+    }
+
 }
--- src/test/java/com/stayready/Problem03Test.java
+++ src/test/java/com/stayready/Problem03Test.java
@@ -1,4 +1,143 @@
 package com.stayready;
 
-public class Problem03Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Problem03Test
+{
+    @Test
+    public void swapCheckTest()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {1,2,3,4,5};
+        Integer numOfSwaps = 2;
+        //When
+        Boolean expected =false;
+        Boolean actual = problem03.swapChecks(array, numOfSwaps);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void swapCheckTest2()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {1,5,3,4,2};
+        Integer numOfSwaps = 2;
+        //When
+        Boolean expected = true;
+        Boolean actual = problem03.swapChecks(array, numOfSwaps);
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void swapCheckTest3()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {2,1,5,3,4};
+        Integer numOfSwaps = 2;
+        //When
+        Boolean expected = false;
+        Boolean actual = problem03.swapChecks(array, numOfSwaps);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsTest()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {3,1,2,4,5};
+        //When
+        Integer expected = 2;
+        Integer actual = problem03.countSwaps(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsTest2()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {1,2,3,4,5};
+        //When
+        Integer expected = 0;
+        Integer actual = problem03.countSwaps(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsTest3()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {2,1,5,3,4};
+        //When
+        Integer expected = 3;
+        Integer actual = problem03.countSwaps(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsTest4()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer [] array = {5,4,3,2,1};
+        //When
+        Integer expected = -1;
+        Integer actual = problem03.countSwaps(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsv2Test()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer [] array = {3,1,2,4,5};
+        //When
+        String expected = "2";
+        String actual = problem03.countSwapsv2(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsv2Test2()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer [] array = {1,2,3,4,5};
+        //When
+        String expected = "0";
+        String actual = problem03.countSwapsv2(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsv2Test3()
+    {
+        Problem03 problem03 = new Problem03();
+        Integer[] array = {2,1,5,3,4};
+        //When
+        String expected = "3";
+        String actual = problem03.countSwapsv2(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void countSwapsv2Test4()
+    {
+        //Given
+        Problem03 problem03 = new Problem03();
+        Integer [] array = {5,4,3,2,1};
+        //When
+        String expected = "Too Chaotic";
+        String actual = problem03.countSwapsv2(array);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
